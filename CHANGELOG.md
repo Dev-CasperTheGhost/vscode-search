fbbcc5a801ee3bde06029812b919995b1fc3c053
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-12-25 20:03:14 +0100

    :tada: NEW: version 1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Change Log
 
+## v1.2
+
+- Fixed small bug when searching non-urls
+- Added settings for changing search engine
+  - DuckDuckGo: default
+  - Google: `search.searchEngine: "https://google.com/search?q="`
+  - Bing: `search.searchEngine: "https://bing.com/search?q="`
+
 ## v1.1
 
 - Added ability to quicker search full URL's by doing: `google.com` instead of `https://google.com`
--- src/extension.ts
+++ src/extension.ts
@@ -2,32 +2,31 @@ import * as vscode from "vscode";
 import tlds from "./tlds";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "vscode-search.search",
-    async () => {
-      const search = await vscode.window.showInputBox({
-        placeHolder: "Enter your search query",
-      });
+  const config = vscode.workspace.getConfiguration("search");
 
-      if (!search) {
-        return;
-      }
-      let url = "";
+  let disposable = vscode.commands.registerCommand("vscode-search.search", async () => {
+    const search = await vscode.window.showInputBox({
+      placeHolder: "Enter your search query",
+    });
 
-      if (search.startsWith("http")) {
-        url = search;
-      } else {
-        const tld = tlds.find((t) => search.includes(t));
-        if (tld) {
-          url = `https://${search}`;
-        } else {
-          url = `https://duckduckgo/${search}`;
-        }
-      }
-
-      vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(url));
+    if (!search) {
+      return;
     }
-  );
+    let url = "";
+
+    if (search.startsWith("http")) {
+      url = search;
+    } else {
+      const tld = tlds.find((t) => search.endsWith(t));
+      if (tld) {
+        url = `https://${search}`;
+      } else {
+        url = `${config.get("searchEngine")}${search}`;
+      }
+    }
+
+    vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(url));
+  });
 
   context.subscriptions.push(disposable);
 }
